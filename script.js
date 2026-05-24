750cd399406be42ba6718d19b2cdc303b140df7e
Joao Carlos <joaocarlosgdln@gmail.com>
2026-05-24 04:52:22 -0300

    new font styles and section nav

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function criarEstrela() {
   const estrela = document.createElement("div");
   estrela.classList.add("estrela");
 
-  const tamanho = Math.random() * 2 + 1;
+  const tamanho = Math.random() * 5 + 1;
   estrela.style.width = `${tamanho}px`;
   estrela.style.height = `${tamanho}px`;
 
   estrela.style.left = `${Math.random() * 100}vw`;
-  const duracao = Math.random() * 5 + 0;
+  const duracao = Math.random() * 15 + 2;
   estrela.style.animationDuration = `${duracao}s`;
   estrela.style.opacity = Math.random();
   ceu.appendChild(estrela);
